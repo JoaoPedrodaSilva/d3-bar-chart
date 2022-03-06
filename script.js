1995647e94141e94402acddf757a5da8a9035bcb
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2022-03-06 15:57:05 -0300

    Little adjustments to use it inside an Iframe somewhere else

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const req = new XMLHttpRequest()
 req.open('GET', 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true)
 req.onload = () => {
   const dataSet = JSON.parse(req.responseText)
+  
   main(dataSet.data)
 }
 req.send()
 
 function main(dataSet) {
+
   const w = 800
   const h = 600
   const pad = 40
@@ -42,28 +44,30 @@ function main(dataSet) {
     .call(yAxis)
 
   d3.select('svg')
-    .attr('width', w)
-    .attr('height', h)
-      .selectAll('rect')
-      .data(dataSet)
-      .enter()
-      .append('rect')
-      .attr('width', barWidth)
-      .attr('height', d => yBarsScale(d[1]))
-      .attr('x', (d, i) => xBarsScale(i))
-      .attr('y', d => h - pad - yBarsScale(d[1]))
-      .attr('data-date', d => d[0])
-      .attr('data-gdp', d => d[1])
-      .attr('class', 'bar')
-      .on('mouseover', (e, d, i) => {
-        tooltip.classList.add('show')
-        tooltip.innerHTML = `${d[0]} <br> U$ ${d[1]} billions`
-        tooltip.setAttribute('data-date', d[0])
-        tooltip.style.left = e.pageX - 150 + 'px'
-        tooltip.style.top = e.pageY - 100 + 'px'
-      })
-      .on('mouseout', () => tooltip.classList.remove('show'))
+  .attr('width', w)
+  .attr('height', h)
+    .selectAll('rect')
+    .data(dataSet)
+    .enter()
+    .append('rect')
+    .attr('width', barWidth)
+    .attr('height', d => yBarsScale(d[1]))
+    .attr('x', (d, i) => xBarsScale(i))
+    .attr('y', d => h - pad - yBarsScale(d[1]))
+    .attr('data-date', d => d[0])
+    .attr('data-gdp', d => d[1])
+    .attr('class', 'bar')
+    .on('mouseover', (e, d, i) => {
+      tooltip.classList.add('show')
+      tooltip.innerHTML = `${d[0]} <br> U$ ${d[1]} billions`
+      tooltip.setAttribute('data-date', d[0])
+      tooltip.style.left = e.pageX - 150 + 'px'
+      tooltip.style.top = e.pageY - 100 + 'px'
+    })
+    .on('mouseout', () => tooltip.classList.remove('show'))
+
 }
+
       
     
 
